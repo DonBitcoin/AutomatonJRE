764f37e8ce0794f8e6291165ca74e556fcf128c7
Akkademic <obeyvanbitcoin@gmail.com>
2023-05-09 19:46:25 +0300

    Candidate to push (Cherry-picked).
    1. Created an normalized document tree.
    2. Captured in List (inside NodeListIterator class) all Nodes of type <book> from Books.xml
    3. Printing all the info contained in child nodes of <book> Nodes

--- src/main/java/org/xml_parsing/XML.java
+++ src/main/java/org/xml_parsing/XML.java
@@ -1,12 +1,13 @@
 package org.xml_parsing;
 
-import org.w3c.dom.*;
+import org.apache.commons.collections4.iterators.NodeListIterator;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-
 import java.io.FileInputStream;
 import java.io.IOException;
 
@@ -15,12 +16,32 @@ public class XML {
     static DocumentBuilderFactory factory;
     static DocumentBuilder builder;
 
-    public static void main(String[] args) throws ParserConfigurationException {
+    public static void main(String[] args) throws ParserConfigurationException
+    {
         factory = DocumentBuilderFactory.newInstance();
         builder = factory.newDocumentBuilder();
+
         try (var input = new FileInputStream(FOLDER + "books.xml"))
         {
             Document document = builder.parse(input);
+            document.getDocumentElement().normalize();
+
+            NodeListIterator nodeIterator = new NodeListIterator(document.getElementsByTagName("book"));
+
+            while (nodeIterator.hasNext())
+            {
+                Node node = nodeIterator.next();
+                Node attribute = node.getAttributes().item(0);
+                System.out.printf("<%s %s>%n", node.getNodeName(), attribute);
+
+                NodeListIterator content = new NodeListIterator(node.getChildNodes());
+                content.forEachRemaining( X -> {
+                    if (! X.getNodeName().matches("#text".toLowerCase()) )
+                        System.out.printf("<%s>\n\t%s\n", X.getNodeName(), X.getTextContent());
+                });
+
+                System.out.println();
+            }
         }
         catch (IOException | SAXException ioe) {
             throw new RuntimeException(ioe);
